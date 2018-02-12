0734a7d4fae282fef67ef3f9b85eb05c8fb2d7dc
Andrew <mackay-andrew@hotmail.com>
2018-02-12 09:45:19 +0000

    User model setup

--- stravaheatmaps/heatmap/models.py
+++ stravaheatmaps/heatmap/models.py
@@ -1,3 +1,16 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
--- stravaheatmaps/heatmap/views.py
+++ stravaheatmaps/heatmap/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from registration.backends.simple.views import RegistrationView
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from heatmap.models import UserProfile
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context_dict = {}
+    try:
+        user = User.objects.get(id=request.user.id)
+        userprofile = UserProfile.objects.get_or_create(user=user)[0]
+        context_dict['userprofile'] = userprofile
+
+    except User.DoesNotExist:
+        pass
+
+    return render(request, "index.html", context=context_dict)
